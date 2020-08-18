1e0016127ef4ee18f0480fb31e68cf913e3cb775
Cauê Alves <caue.mendes.rodrigues.alves@gmail.com>
2020-08-18 18:23:30 -0300

    Add files via upload

--- SeletorDeTabelas.sql
+++ SeletorDeTabelas.sql
@@ -5,3 +5,23 @@ SELECT * FROM tbcliente;
 
 SELECT PRODUTO FROM tbcliente LIMIT 5;
 # (LIMITA O RETORNO A 5 ITENS)
+
+SELECT PRODUTO FROM tbcliente WHERE PRECO_LISTA <= 5;
+# Seleciona todos os itens com preço maior igual a 5
+
+SELECT PRODUTO FROM tbcliente WHERE PRECO_LISTA <> 10;
+# Seleciona todos os itens com preço diferente de 10
+
+SELECT CLIENTE FROM tbcliente WHERE YEAR(DATA_NACSIMENTO) = 1998;
+# Seleciona o cliente com data de nascimento no ano de 1998
+
+SELECT PRODUTO FROM tbproduto WHERE PRECO_LISTA BETWEEN 5 AND 10;
+# Seleciona um produto cujo preço está entre 5 e 10
+
+SELECT PRODUTO FROM tbproduto WHERE PRECO_LISTA > 5 AND EMBALAGEM = 'PET';
+#Seleciona um produto que é mais caro que 5 e sua embalagem é PET
+
+SELECT PRODUTO FROM tbproduto WHERE PRECO_LISTA > 5 OR EMBALAGEM = 'PET';
+#Seleciona um produto que é mais caro que 5 ou sua embalagem é PET
+
+
